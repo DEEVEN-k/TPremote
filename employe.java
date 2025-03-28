1ceb502256f93f0e613a64e87eeb0923185f9e9d
DEEVEN <eyadomaleki@gmail.com>
2025-03-28 21:30:03 +0000

    commit 1

---
+++ employe.java
@@ -0,0 +1,23 @@
+public class employe {
+    private int id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private char phone;
+    private String imageUrl;
+    private int employeCode;
+    private String dateCreation;
+
+    public employe(int id, String name, String email, String jobTitle, char phone, String imageUrl, int employeCode, String dateCreation) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.jobTitle = jobTitle;
+        this.phone = phone;
+        this.imageUrl = imageUrl;
+        this.employeCode = employeCode;
+        this.dateCreation = dateCreation;
+    }
+}
+
+
